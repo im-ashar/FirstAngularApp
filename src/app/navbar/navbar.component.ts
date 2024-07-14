cf0310f9fe472862413b74ee48db32f9f46348a2
im-ashar <muhammadashar.401@gmail.com>
2024-07-14 10:07:49 +0500

    connected external html and css file with component.ts file

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -3,7 +3,8 @@ import { Component } from "@angular/core";
 @Component({
     selector: 'app-navbar',
     standalone: true,
-    template: '<h1>Navbar Component</h1>'
+    templateUrl: './navbar.component.html',
+    styleUrl: './navbar.component.css'
 })
 
 export class AppNavbar {
