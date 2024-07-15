8f747dcfb953987c81f5870b9ee2edc41efe3be4
im-ashar <muhammadashar.401@gmail.com>
2024-07-15 09:39:52 +0500

    learn one way and two way binding

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,14 +1,34 @@
 import { Component } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
   heading: string = 'This is Loaded Dynamically'
   imgUrl: string = 'https://images.ctfassets.net/zojzzdop0fzx/1HXhc8fHDYeYwp7xuBUsxb/3c8fc6aa1f44e68e455def8011585b5d/j0f6eq.png';
-  isDisabled: boolean = true;
+  isDisabled: boolean = false;
+  isActive: boolean = true;
+  textValue: string = 'Ashar Bhatti';
+
+  buttonClick() {
+    console.log('Button Clicked');
+    this.isDisabled = !this.isDisabled;
+  }
+
+  keyPressed(event: KeyboardEvent) {
+    if (event.key == 'Enter') {
+      alert(this.textValue);
+    }
+  }
+  keyPressed2() {
+    alert(this.textValue);
+  }
+  keyPressed3(userName: HTMLInputElement) {
+    this.textValue = userName.value;
+    alert(this.textValue);
+  }
 }
